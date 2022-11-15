97950206c9b21d0e7f9657d9d8354a0716f17c1e
lmatheny <97480690+lmatheny@users.noreply.github.com>
2022-11-15 02:16:05 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,49 @@
+required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.0"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+provider "azurerm" {
+  features {}
+}
+
+
+
+# Create the resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "TerraformGeneratedResourceGroup"
+  location = "eastus"
+}
+
+# Create the Linux App Service Plan
+resource "azurerm_service_plan" "appserviceplan" {
+  name                = "TerraformGeneratedAppServicePlan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+# Create the web app, pass in the App Service Plan ID
+resource "azurerm_linux_web_app" "webapp" {
+  name                  = "TerraformGeneratedWebApp"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  service_plan_id       = azurerm_service_plan.appserviceplan.id
+  https_only            = true
+  site_config { 
+    minimum_tls_version = "1.2"
+  }
+}
+
+#  Deploy code from my public GitHub repo
+resource "azurerm_app_service_source_control" "sourcecontrol" {
+  app_id             = azurerm_linux_web_app.webapp.id
+  repo_url           = "https://github.com/lmatheny/MyVersion"
+  branch             = "master"
+  use_manual_integration = true
+  use_mercurial      = false
+}
